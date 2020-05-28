1a224960070271b26bbc4e3ddf819e4e1cc7dc5c
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-05-28 10:22:32 +0530

    updating date and layout

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
-let items = ["buy food", "cook food"];
-let workItems = [];
+const items = ["buy food", "cook food"];
+const workItems = [];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
 
-    let day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
   res.render("list", { listTitle: day, newListItems:items });
 });
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
